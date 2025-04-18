65d4c095e2a591cf88ffb90ab9a0b0a6d61d9ff0
Satoru Zati <satadrujati3317@gmail.com>
2025-04-19 00:36:54 +0530

    Error Page

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -5,8 +5,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Signup from './pages/Signup'
 import Login from './pages/Login'
 import Dashboard from './Components/Dashboard'
-
 import LandingPage from './pages/LandingPage'
+import ErrorRoute from './pages/ErrorRoute'
 
 function App() {
   return <StateProvider>
@@ -16,6 +16,7 @@ function App() {
             {<Route path='/signin' element = {<Login/>}/>}
             {<Route path='/dashboard' element = {<Dashboard/>}/>}
             {<Route path='/' element = {<LandingPage/>}/>}
+            {<Route path='*' element = {<ErrorRoute/>}/>}
         </Routes>
     </BrowserRouter>
   </StateProvider>

3e0d8d9a9b39310a9664ebce6d59e90f2dbcb42c
Satoru Zati <satadrujati3317@gmail.com>
2025-04-18 21:08:45 +0530

    Pushing Landing Page
    Created Landing Page and updated routes.

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,15 +1,17 @@
 import './Global.css'
 import { StateProvider } from './Context API/StateContext'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Signup from './pages/Signup'
-import Login from './pages/Login'
+import Signup from './Pages/Signup'
+import Login from './Pages/Login'
+import LandingPage from './Pages/LandingPage'
        
 function App() {
   return <StateProvider>
     <BrowserRouter>
         <Routes>
-            {<Route path='/' element = {<Signup/>}/>}
+            {<Route path='/signup' element = {<Signup/>}/>}
             {<Route path='/signin' element = {<Login/>}/>}
+            {<Route path='/' element = {<LandingPage/>}/>}
         </Routes>
     </BrowserRouter>
   </StateProvider>

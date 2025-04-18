be15798fffa71f1111164c8f78432b59713c225f
Is-hi-ka <ishika.sahajuly21@gmail.com>
2025-04-18 18:32:32 +0530

    added the Context API

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,17 +1,18 @@
 import './Global.css'
+import { StateProvider } from './Context API/StateContext'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Signup from './pages/Signup'
 import Login from './pages/Login'
        
 function App() {
-  return (
+  return <StateProvider>
     <BrowserRouter>
         <Routes>
             {<Route path='/' element = {<Signup/>}/>}
             {<Route path='/signin' element = {<Login/>}/>}
         </Routes>
     </BrowserRouter>
-  )
+  </StateProvider>
 }
 
 export default App
